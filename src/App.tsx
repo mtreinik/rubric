6169f25e5ecaf8167e65be9d19fe8f5b9472fd72
Mikko Reinikainen <mikko.reinikainen@reaktor.com>
2020-08-22 20:06:31 +0300

    Implement clearing, selecting, unselecting and copying content

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,52 @@ const theme = createMuiTheme({
   }
 })
 
+const reload = () => {
+  document.location.reload()
+}
+
+const setSelectionEnabled = (enabled: boolean) => {
+  const elementId = 'content'
+  const element = document.getElementById(elementId)
+  if (!element) {
+    console.error('could not get element', elementId)
+    return
+  }
+  element.className = enabled ? '' : 'noselect'
+}
+
+const unselectAll = () => {
+  const selection = window.getSelection()
+  if (!selection) {
+    return
+  }
+  selection.removeAllRanges()
+  setSelectionEnabled(false)
+}
+
+const selectElement = (elementId: string) => () => {
+  const element = document.getElementById(elementId)
+  if (!element) {
+    console.error('could not get element', elementId)
+    return
+  }
+  const range = document.createRange()
+  range.selectNodeContents(element)
+  const selection = window.getSelection()
+  if (!selection) {
+    return
+  }
+  selection.removeAllRanges()
+  setSelectionEnabled(true)
+  selection.addRange(range)
+  document.execCommand("copy")
+}
+
 const App = () => {
+  React.useEffect(() => {
+    setSelectionEnabled(false)
+  })
+
   return <React.Fragment>
     <CssBaseline/>
     <ThemeProvider theme={theme}>
@@ -23,15 +68,26 @@ const App = () => {
           <h1>Rubric</h1>
         </Grid>
         <Grid item xs={12}>
-          <Clickable value="kiinnostava"/>
-          <span> / </span>
-          <Clickable value="omaperäinen"/>
+          <div id="content">
+            <div>&nbsp;</div>
+            <div>
+              <Clickable value="kiinnostava"/>
+              <span> / </span>
+              <Clickable value="omaperäinen"/>
+            </div>
+          </div>
         </Grid>
         <Grid item xs={12}>
           <Grid container spacing={4}>
-            <Grid item><Button variant="outlined" color="secondary">Tyhjennä</Button></Grid>
-            <Grid item><Button variant="contained">Poista valinta</Button></Grid>
-            <Grid item><Button variant="contained" color="primary">Valitse ja kopioi</Button></Grid>
+            <Grid item>
+              <Button onClick={reload} variant="outlined" color="secondary">Tyhjennä</Button>
+            </Grid>
+            <Grid item>
+              <Button onClick={unselectAll} variant="contained">Poista valinta</Button>
+            </Grid>
+            <Grid item>
+              <Button onClick={selectElement('content')} variant="contained" color="primary">Valitse ja kopioi</Button>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
