092c43ed23cfca7bf4919e12a47ed826efe21475
Mikko Reinikainen <mikko.reinikainen@reaktor.com>
2020-08-22 18:24:15 +0300

    Add material UI and roboto font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
 import * as React from 'react'
+import {AppBar, Button, CssBaseline, Grid} from '@material-ui/core/'
+import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {main: '#2e7d32'},
+    secondary: {main: '#ff1744'}
+  }
+})
 
 const App = () => {
-  return <div>app goes here</div>
+  return <React.Fragment>
+    <CssBaseline/>
+    <ThemeProvider theme={theme}>
+      <AppBar position="static">
+        TODO toolbar
+      </AppBar>
+
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          TODO content
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container spacing={8}>
+            <Grid item><Button variant="outlined" color="secondary">Tyhjennä</Button></Grid>
+            <Grid item><Button variant="contained">Poista valinta</Button></Grid>
+            <Grid item><Button variant="contained" color="primary">Valitse ja kopioi</Button></Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  </React.Fragment>
 }
 
 export default App
