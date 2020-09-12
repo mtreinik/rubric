2e30ad2d7b30e980c90db18f988253d6881aa142
Mikko Reinikainen <mikko.reinikainen@reaktor.com>
2020-09-12 15:46:07 +0300

    Improve layout

--- src/InfoView.tsx
+++ src/InfoView.tsx
@@ -4,6 +4,8 @@ interface Props {
   title: string
 }
 
-const InfoView = (props: Props): JSX.Element => <div>{props.title}</div>
+const InfoView = (props: Props): JSX.Element => (
+  <div className="infoCriterion">{props.title}</div>
+)
 
 export default InfoView
