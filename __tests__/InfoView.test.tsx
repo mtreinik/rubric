04aa46ec6a57914f1bc613152ec40db5bd0d6e20
Mikko Reinikainen <mikko.reinikainen@reaktor.com>
2020-09-18 10:53:43 +0300

    Support HTML in info content
    - XSS attacks are sanitized from info content with DOMPurify

---
+++ __tests__/InfoView.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import InfoView from '../src/InfoView'
+
+describe('InfoView renders correctly with', () => {
+  const expectValidComponentWithTitle = (title: string): void => {
+    const component = renderer.create(<InfoView title={title}></InfoView>)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  }
+  it('empty title', () => {
+    expectValidComponentWithTitle('')
+  })
+  it('text title', () => {
+    expectValidComponentWithTitle('xyzzy')
+  })
+  it('HTML title', () => {
+    expectValidComponentWithTitle('this is <strong>heavy</strong>')
+  })
+  it('XSS attack sanitized from title', () => {
+    expectValidComponentWithTitle(
+      '<span onmouseover="alert(document.cookie)">this is an XSS attack</span>'
+    )
+  })
+})
--- src/InfoView.tsx
+++ src/InfoView.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import DOMPurify from 'dompurify'
 
 interface Props {
   title: string
 }
 
-const InfoView = (props: Props): JSX.Element => (
-  <div
-    className="infoCriterion"
-    dangerouslySetInnerHTML={{ __html: props.title }}
-  />
-)
-
+const InfoView = (props: Props): JSX.Element => {
+  const sanitizedHTML = DOMPurify.sanitize(props.title)
+  return (
+    <div
+      className="infoCriterion"
+      dangerouslySetInnerHTML={{ __html: sanitizedHTML }}
+    />
+  )
+}
 export default InfoView
