fccbd809e13b1ebb9ba1fdb22b2db067d84b9772
Mikko Reinikainen <mikko.reinikainen@reaktor.com>
2020-09-19 13:09:32 +0300

    Show borders between slider columns

--- client/src/SliderView.tsx
+++ client/src/SliderView.tsx
@@ -16,22 +16,29 @@ const SliderView = (props: Props): JSX.Element => {
       {option}
     </td>
   ))
+  const cellsPerColumn = 5
 
   const rows = props.rows.map((row, rowIndex) => (
     <tr key={'row-' + rowIndex}>
-      <td>{row.title}</td>
-      <td colSpan={props.options.length} style={{ fontFamily: 'Monospace' }}>
-        {Array.from({ length: 40 }).map((_, value) => (
-          <SliderCell
-            key={'option-' + rowIndex + '-' + value}
-            rowIndex={rowIndex}
-            selectedValue={row.value}
-            value={value}
-            setSliderRowValue={props.setSliderRowValue}
-            selection={props.selection}
-          />
-        ))}
-      </td>
+      <td className="sliderRowHeader">{row.title}</td>
+      {props.options.map((option, optionIndex) => (
+        <td
+          className="sliderColumn"
+          style={{ fontFamily: 'Monospace' }}
+          key={'column-' + optionIndex}
+        >
+          {Array.from({ length: cellsPerColumn }).map((_, value) => (
+            <SliderCell
+              key={'cell-' + value}
+              rowIndex={rowIndex}
+              selectedValue={row.value}
+              value={optionIndex * cellsPerColumn + value}
+              setSliderRowValue={props.setSliderRowValue}
+              selection={props.selection}
+            />
+          ))}
+        </td>
+      ))}
     </tr>
   ))
 
